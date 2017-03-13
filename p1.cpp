3adf6d30403ce3e777239f15316cf7d53c5b61e9
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2017-03-13 10:02:52 +0000

    comments fixed

--- p1.cpp
+++ p1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 typedef vector <int>::reverse_iterator vri;
 typedef vector <int>::iterator vi;
 
-vector<vector<int> > G; //grafo
+vector<vector<int> > G;//grafo
 vector<int> ts;//ordem topologica
 vector<bool> visited;//vertices visitados
 vector<bool> cycleStack;//ciclos no grafo
@@ -20,7 +20,7 @@ bool uniqueTopSort = true;//Tem uma ordenacao topologica unica?
 int N, L;
 
 /* dfs: Visita todos os vertices de um grafo, e deteta existencia de ciclos
- * Devolve: nada
+ * Devolve: void
  * Ordenacao topologica baseada em pesquisa em profundidade (DFS)
  */
 void dfs(int u) {
@@ -62,18 +62,18 @@ int main() {
 		printf("Incoerente\n");
 	} else {
 		/* Verifica se dois vertices consecutivos na ordenacao topologica
-		estao ligados entre si. Se nao estiverem entao nao existe uma ordem unica */
-		for (vector <int>::reverse_iterator v = ts.rbegin(); v + 1 != ts.rend(); v++) {
+		estao ligados entre si. Se nao estiverem entao nao formam um caminho Hamiltoniano.
+		Se formarem um caminho Hamiltoniano entao e garantido que tem apenas uma ordenacao topologica unica */
+		for (vri v = ts.rbegin(); v + 1 != ts.rend(); v++) {
 			if (find(G[*v].begin(), G[*v].end(), *(v + 1)) == G[*v].end()) {
 				uniqueTopSort = false;
 				break;
 			}
 		}
-		/* Se existir uma ordenacao topologica unica imprime os resultados,
-		caso contrario nao e possivel imprimir uma ordem unica */
+		/* Se existir uma ordenacao topologica unica imprime os resultados */
 		if (uniqueTopSort) {
 			for (vri v = ts.rbegin(); v != ts.rend(); v++)
-				cout << *v + 1 << (v + 1 == ts.rend() ? "\n" : " ");
+				printf("%d%s", *v + 1, v + 1 == ts.rend() ? "\n" : " ");
 		} else {
 			printf("Insuficiente\n");
 		}
